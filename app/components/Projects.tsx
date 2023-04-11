d659e8d9f63c233f4ba342a67ef18510199cc9cc
李大毛 <dev@darmau.design>
2023-04-11 23:15:30 +0800

    style: improvement

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 type Portfolio = {
@@ -29,18 +30,31 @@ export default function Projects({ portfolios }: { portfolios: Portfolio[] }) {
         <div className="md:col-span-1 md:flex md:items-center md:justify-center">
           <ul className="space-y-4">
             {portfolios.map((portfolio: Portfolio, index: number) => {
-              const hoverStyle = index === hoverIndex ? 'border border-black bg-white/40 backdrop-blur-lg' : 'opacity-50 border border-transparent';
+              const hoverStyle =
+                index === hoverIndex
+                  ? "border border-black bg-white/40 backdrop-blur-lg"
+                  : "opacity-50 border border-transparent";
               return (
                 <li
                   key={index}
-                  className={`flex items-center space-x-4 cursor-pointer p-4 box-content transition-all	duration-300 ${hoverStyle}`}
+                  className={`cursor-pointer box-content transition-all	duration-300 ${hoverStyle}`}
                   onMouseEnter={() => setHoverIndex(index)}
                 >
-                  <div className="text-6xl font-title">{`0${index+1}.`}</div>
-                  <div className="space-y-2">
-                    <h2 className="font-serif font-bold text-xl">{portfolio.attributes.title}</h2>
-                    <p className="text-gray-600 text-sm">{portfolio.attributes.description}</p>
-                  </div>
+                  <Link 
+                  className="flex items-center space-x-4 p-4 "
+                  href={`/${portfolio.attributes.slug}`}>
+                    <div className="text-6xl font-title">{`0${
+                      index + 1
+                    }.`}</div>
+                    <div className="space-y-2">
+                      <h2 className="font-serif font-bold text-xl">
+                        {portfolio.attributes.title}
+                      </h2>
+                      <p className="text-gray-600 text-sm">
+                        {portfolio.attributes.description}
+                      </p>
+                    </div>
+                  </Link>
                 </li>
               );
             })}
